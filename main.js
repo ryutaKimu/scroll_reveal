368b3d9d42b310474a337abee617006fa46f5bb6
kimryu <test@tesutonoMacBook-Air.local>
2024-05-30 08:01:24 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+ScrollReveal({ reset: true,distance:"60px",duration:2000,delay:200});
+
+ScrollReveal().reveal('.main-title,section-title',{delay:200,origin:"left"});
+ScrollReveal().reveal('.sec-01 .image,.info',{delay:600,origin:"bottom"});
+ScrollReveal().reveal('.text-box',{delay:700,origin:"right"});
+ScrollReveal().reveal('.media-icons i',{delay:500,origin:"bottom",interval:200});
+ScrollReveal().reveal('.sec-02 .image',{delay:500,origin:"top"});
+ScrollReveal().reveal('.media-info li',{delay:500,origin:"left",interval:200});
